78e855909a307fd4cd1fa9206341a827e38337be
SebastienBosca <sbosca@wanadoo.fr>
2022-05-06 12:28:19 +0200

    Updates

---
+++ precache-manifest.9c3db672905b47ec286f8bc5da3c10cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52840f6f4cf5106ecc172a3a23bb59af",
+    "url": "/STAKING/index.html"
+  },
+  {
+    "revision": "4c6c5d9bd7f0c253b8c6",
+    "url": "/STAKING/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "c082ae4211dcb46c6920",
+    "url": "/STAKING/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "4c6c5d9bd7f0c253b8c6",
+    "url": "/STAKING/static/js/2.42e521a8.chunk.js"
+  },
+  {
+    "revision": "c082ae4211dcb46c6920",
+    "url": "/STAKING/static/js/main.e6c42f05.chunk.js"
+  },
+  {
+    "revision": "812935d27ee5d4c49f0f",
+    "url": "/STAKING/static/js/runtime-main.62a3c73a.js"
+  }
+]);
